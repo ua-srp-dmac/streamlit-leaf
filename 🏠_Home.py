3db8951dd1716c8850924e53c9b798256997a079
Michelle Yung <myung@email.arizona.edu>
2022-08-25 18:45:31 -0700

    cleanup

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -5,46 +5,32 @@ setup_logger()
 # import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
+from detectron2.data.catalog import Metadata
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer, ColorMode
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.data.datasets import register_coco_instances
-from detectron2.data.catalog import Metadata
-from detectron2.modeling import build_model
-from detectron2.checkpoint import DetectionCheckpointer
-from pathlib import Path
 
-import requests
-import argparse
-
-import os, json, random
-
-import streamlit as st
-import mediapipe as mp
+import os
 import cv2
 import numpy as np
-import tempfile
-import time
-from PIL import Image
 import pandas as pd
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from streamlit_option_menu import option_menu
-
+from PIL import Image
+from pathlib import Path
 from pyzbar.pyzbar import decode
 from pyzbar.pyzbar import ZBarSymbol
-
 from torchvision import transforms
+
+import streamlit as st
+
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
 import sys
 
-
-DEMO_IMAGE = '250-8.JPG'
 
 st.title('Leaf Segmentation')
 
 # get base data path from user input
 base_path = sys.argv[1]
-
 
 leaf_cfg = get_cfg()
 leaf_cfg.MODEL.DEVICE='cpu'
@@ -56,33 +42,16 @@ leaf_cfg.SOLVER.IMS_PER_BATCH = 2
 leaf_cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
 leaf_cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
 leaf_cfg.SOLVER.STEPS = []        # do not decay learning rate
-leaf_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (qrcode). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-leaf_cfg.MODEL.WEIGHTS = base_path + "models/leaf_model.pth"  # path to the model we just trained
+leaf_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # only has one class (qrcode). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+leaf_cfg.MODEL.WEIGHTS = base_path + "models/leaf_qr_model.pth"  # path to the model we just trained
 leaf_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
 
 
 leaf_predictor = DefaultPredictor(leaf_cfg)
 
-# qr_cfg = get_cfg()
-# qr_cfg.MODEL.DEVICE='cpu'
-# qr_cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-# qr_cfg.DATASETS.TEST = ()
-# qr_cfg.DATALOADER.NUM_WORKERS = 2
-# qr_cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-# qr_cfg.SOLVER.IMS_PER_BATCH = 2
-# qr_cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-# qr_cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-# qr_cfg.SOLVER.STEPS = []        # do not decay learning rate
-# qr_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (qrcode). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-# qr_cfg.MODEL.WEIGHTS = "qr_model.pth"  # path to the model we just trained
-# qr_cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
-
-# os.makedirs(qr_cfg.OUTPUT_DIR, exist_ok=True)
-
-# qr_predictor = DefaultPredictor(qr_cfg)
 
 leaf_metadata = Metadata()
-leaf_metadata.set(thing_classes = ['leaf'])
+leaf_metadata.set(thing_classes = ['leaf', 'qr', 'red-square'])
 
 if not os.path.isdir(base_path + 'results'):
     os.mkdir(base_path + 'results') 
@@ -129,11 +98,11 @@ def run_inference(batch):
     for index, image in enumerate(batch):
 
         leaf_outputs = leaf_predictor(image["image"])
-        # qr_outputs = qr_predictor(image["image"])
-        # pred_boxes = leaf_outputs["instances"].pred_boxes
-        # print("Found " + str(len(pred_boxes)) + " leaves")
+        # print(leaf_outputs)
+        pred_boxes = leaf_outputs["instances"].pred_boxes
+        print("Found " + str(len(pred_boxes)) + " leaves")
 
-        # pred_boxes = qr_outputs["instances"].pred_boxes
+        print(pred_boxes)
 
         qr_result_decoded = None
 
@@ -194,8 +163,6 @@ gd.configure_column("File Name", headerCheckboxSelection = True)
 
 file_table = AgGrid(df, fit_columns_on_grid_load=True, gridOptions=gd.build(), update_mode=GridUpdateMode.SELECTION_CHANGED)
 
-# st.sidebar.text('Original Image')
-# st.sidebar.image(image)
 run = st.button('Run')
 
 if run:
@@ -213,16 +180,6 @@ if run:
     
     run_inference(batch)
 
-    # main_menu = "Results"
-
-    # leaf_count = 0
-
-    # if img_file_buffer is not None:
-    #     image = np.array(Image.open(img_file_buffer))
-
-    # else:
-    #     demo_image = DEMO_IMAGE
-    #     image = np.array(Image.open(demo_image))
     
 
 
