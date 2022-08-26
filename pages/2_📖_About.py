5d1a9e0391d8bcf9c4caade1178371fa042ff0b7
Michelle Yung <myung@email.arizona.edu>
2022-08-25 20:55:27 -0700

    update about page

--- pages/2_📖_About.py
+++ pages/2_📖_About.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.markdown('In this application we are using **MediaPipe** for creating a Face Mesh. **StreamLit** is to create the Web Graphical User Interface (GUI) ')
+st.markdown('In this application we are using **Detectron2** for leaf segmentation. **StreamLit** is to create the Web Graphical User Interface (GUI) ')
